44e16369fe512e3070f7605e991fd52b58a20aa4
Sam Saccone <samccone@gmail.com>
2015-05-27 13:28:42 -0400

    :goat:

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var request = require('request-promise');
+var track = 1;
+
+if (process.argv[2] && process.argv[2].toLowerCase() == 'b') {
+  console.log("✨ Streaming track B✨ ");
+  track = 2;
+} else {
+  console.log("✨ Streaming track A✨ ");
+}
+
+(function tick(stamp) {
+  request.get('http://www.streamtext.net/text-data.ashx?event=JSConf' + track +  '&last=' + stamp + '&language=en')
+  .then(function(v) {
+    try {
+      v = JSON.parse(v);
+      stamp = v.lastPosition;
+      if (v.i && v.i.length) {
+        process.stdout.write(v.i.map(function(v) {
+          return decodeURIComponent(v.d);
+        }).join(''));
+    }
+    } catch (e) {
+    } finally {
+      setTimeout(tick.bind({}, stamp), 1000);
+    }
+  });
+})(null);
+
